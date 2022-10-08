75a56b550d4a7f36423e4e131a676b93890677c2
prathamy <pra.yeole@gmail.com>
2022-10-08 11:27:23 +0530

    extended commit

--- f1.py
+++ f1.py
@@ -4,3 +4,5 @@ a = "this is change in master branch to see merge"
 """======="""
 a = "conflicts in new-feature to see merge conflicts"
 """>>>>>>> new-feature"""
+
+a = "some changes in the xtended branch"
